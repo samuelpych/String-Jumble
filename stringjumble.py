ad504fc86fac968c675b7a5fb49059b6750ddb12
samuelpych <samuelpych@hanovernorwichschools.org>
2017-03-06 14:25:01 -0500

    Updated from Brython Server: 3/6/2017 2:25:01 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,7 +26,7 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 mix=str(input("Please enter a string of text (the bigger the better): "))
-print("You entered"+mix+". Now jumble it:")
+print("You entered \""+mix+"\". Now jumble it:")
 
 print((mix)[len(mix)::-1])
 lastspace=len(mix)
